a3bdd9f0991d55bae399d92ad18cf5208c2286c1
jonezra312 <jonezra312@gmail.com>
2025-01-07 15:59:30 +0000

    added class work

---
+++ game.py
@@ -0,0 +1,8 @@
+import pyglet
+
+window_width = 800
+window_height = 600
+
+window =pyglet.window.window(height=window_height, width=window_width, caption="My Game")
+
+pyglet.app.run()
--- hello-test.py
+++ hello-test.py
@@ -1,6 +1,6 @@
 import pyglet
-window = pyglet.window.Window()
-label = pyglet.text.Label('Hello, Jon Ezra Plowman', font_size=36, x=200, y=100)
+window = pyglet.window.Window(width=960, height=540)
+label = pyglet.text.Label('Hello, Jon Ezra', font_size=36, x=20, y=490)
 
 @window.event
 def on_draw():
