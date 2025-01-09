f490c8a1fce4879931e197e7447f224e4dc57fa2
jonezra312 <jonezra312@gmail.com>
2025-01-09 13:10:30 +0000

    added homework

--- game.py
+++ game.py
@@ -3,6 +3,6 @@ import pyglet
 window_width = 800
 window_height = 600
 
-window =pyglet.window.window(height=window_height, width=window_width, caption="My Game")
+window = pyglet.window.Window(height=window_height, width=window_width, caption="My Game")
 
 pyglet.app.run()
