c3857bf84e0ee841abf92e6bb4c5dacbd43e3e9d
jonezra312 <jonezra312@gmail.com>
2025-02-06 15:46:54 +0000

    added files

--- coding fun.py
+++ coding fun.py
@@ -34,16 +34,26 @@ player = Circle(player_x,player_y,player_radius, color=WHITE)
 player_speed = 150
 player_dir = 'left'
 def update(dt):
-    #global player_dir
+    
     # if player_dir == 'right' and player.x > path_x + path_width - player_radius:
     #     player_dir = 'left'
     # elif player_dir == 'left' and player.x < path_x + player_radius:
     #     player_dir = 'right'
 
-    if keys[key.LEFT]:
-        player.x-=player_speed * dt
+    # if player_dir == 'right':
+    #     player.x = player.x + player_speed * dt
+    # if player_dir == 'left':
+    #     player.x = player.x - player_speed * dt
+   #controls     
+    def update(dt):
+        if keys[key.LEFT]:
+         player.x-=player_speed * dt
     if keys[key.RIGHT]:
         player.x+=player_speed * dt
+    if keys[key.UP]:
+        player.y+=player_speed * dt
+    if keys[key.DOWN]: 
+        player.y-=player_speed * dt
     
     # if player_dir == 'right':
     #     player.x = player.x + player_speed * dt
@@ -56,9 +66,10 @@ def update(dt):
     # elif player_dir == 'left' and player.x < path_x + player_radius:
     #     player_dir = 'right'
 
-@window.event
-def on_draw():
-    window.clear()
+ #how the app runs
+        @window.event
+        def on_draw():
+            window.clear()
     green_background.draw()
     path.draw()
     player.draw()
