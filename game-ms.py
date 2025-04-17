eddc0ba1e8795beed72476a6a2cca79d6fcb17e0
jonezra312 <jonezra312@gmail.com>
2025-04-17 13:17:22 +0000

    added files

--- game-ms.py
+++ game-ms.py
@@ -1,10 +1,13 @@
 import pyglet
-
 from pyglet.shapes import Rectangle, Circle
-from pyglet.window import Window, key 
-print(key.LEFT)
+from pyglet.window import Window, key
+from pyglet.text import Label
+from pyglet.image import load as loadImage 
+from pyglet.sprite import Sprite
 
-#Window config 
+from random import randint
+
+# Window config
 window_width = 800
 window_height = 600
 window = Window(height=window_height, width=window_width, caption="My Game")
@@ -12,56 +15,150 @@ window = Window(height=window_height, width=window_width, caption="My Game")
 keys = key.KeyStateHandler()
 window.push_handlers(keys)
 
-# colors
+# Colors
 GRAY=(170,170,170)
 GREEN = (34,139,34)
 WHITE = (255,255,255)
+RED = (255,0,0)
 
 # path config
 path_width = 400
 path_x = (window_width-path_width)/2
 path = Rectangle(path_x,0,path_width,window_height,GRAY)
 
-#backround
+# backgronud
 green_background = Rectangle(0,0, window_width, window_height, GREEN)
 
-#player config
+# player config
 player_y = 100
-player_x = window_width/2
+player_x = window_width / 2
 player_radius = 40
-
-player = Circle(player_x,player_y,player_radius, color=WHITE)
+player_image = loadImage("tank_huge_1.png")
+player = Sprite(player_image,x=player_x, y=player_y)
+player.scale = 0.4
+#player = Circle(player_x,player_y,player_radius, color=WHITE )
 player_speed = 150
-player_dir = 'left'
+
+obstacle_size = 40
+obstacle_speed = 200
+
+obstacles = []
+obstacle_image = loadImage ("barricadeWood.png")
+def add_obstacle(dt):
+    x_pos = randint(
+        int(path.x+obstacle_size), 
+        int(path.x + path_width - obstacle_size)
+        )
+    obstacle = Sprite(obstacle_image, x_pos, y=Window)
+    obstacle = Rectangle(x_pos,window_height-obstacle_size, obstacle_size, obstacle_size,RED)
+    obstacles.append(obstacle)
+
+points = 0
+points_label = Label("Points: 0", font_name="Arial",font_size=18,
+                     x=10, y=window_height-30)
+
+def update_points(dt):
+    global points
+    points += 1
+    points_label.text = f'Points: {points}'
+
+health = 100
+health_label = Label(
+    f'Health: {health}', 
+    font_name='Arial', 
+    font_size=18, 
+    x=10, 
+    y=window_height - 60
+)
+
+game_over = False
+game_over_label = Label(
+    'Game Over',
+    font_size=36,
+    color=RED,
+    x=window_width / 2,
+    anchor_x='center',
+    y=window_height / 2,
+    anchor_y='center'
+)
+
+def check_collision():
+    for obstacle in obstacles:
+        if(
+            player.x - player_radius < obstacle.x + obstacle.width and
+            player.x + player_radius > obstacle.x and
+            player.y - player_radius < obstacle.y + obstacle.height and
+            player.y + player_radius > obstacle.y
+        ):
+            return obstacle
+    return None
+
 def update(dt):
-    #global player_dir
-    # if player_dir == 'right' and player.x > path_x + path_width - player_radius:
-    #     player_dir = 'left'
-    # elif player_dir == 'left' and player.x < path_x + player_radius:
-    #     player_dir = 'right'
 
     if keys[key.LEFT]:
-        player.x-=player_speed * dt
+        player.x -= player_speed * dt
+
     if keys[key.RIGHT]:
-        player.x+=player_speed * dt
-    
-    # if player_dir == 'right':
-    #     player.x = player.x + player_speed * dt
-    # if player_dir == 'left':
-    #     player.x = player.x - player_speed * dt
+        player.x += player_speed * dt
 
+    if player.x > path_width+path_x-player_radius:
+        player.x =  path_width+path_x-player_radius
 
-    # if player_dir == 'right' and player.x > path_x + path_width - player_radius:
-    #     player_dir = 'left'
-    # elif player_dir == 'left' and player.x < path_x + player_radius:
-    #     player_dir = 'right'
+    if player.x < path_x+player_radius:
+        player.x = path_x + player_radius
+
+    for obstacle in obstacles[:]:
+        obstacle.y -= obstacle_speed * dt
+        if obstacle.y + obstacle_size < 0:
+            obstacle.delete()
+            obstacles.remove(obstacle)
+    obstacle_hit = check_collision()
+    global health
+    if obstacle_hit:
+        health -= 10
+        health_label.text = f'Health: {health}'
+        obstacles.remove(obstacle_hit)
+        obstacle_hit.delete()
+
+    if health <= 0:
+        global game_over
+        game_over = True
+        pyglet.clock.unschedule(update)
+        pyglet.clock.unschedule(update_points)
+        pyglet.clock.unschedule(add_obstacle)
 
 @window.event
 def on_draw():
+    global game_over
     window.clear()
     green_background.draw()
     path.draw()
     player.draw()
+    for obstacle in obstacles[:]:
+        obstacle.draw()
 
-pyglet.clock.schedule_interval(update, 1/60.0)
+    points_label.draw()
+    health_label.draw()
+    if game_over:
+        game_over_label.draw()
+
+@window.event
+def on_mouse_press(x,y,button, modifiers):
+    global game_over, health, points, obstacles
+    if game_over:
+        game_over = False
+        health = 100
+        points = 0
+        obstacles.clear()
+        add_obstacle(1)
+        health_label.text = f'Health: {health}'
+        points_label.text =f'Points: {points}'
+        pyglet.clock.schedule_interval(update_points,1)
+        pyglet.clock.schedule_interval(add_obstacle, 2)
+        pyglet.clock.schedule_interval(update, 1/60)
+
+add_obstacle(1)
+pyglet.clock.schedule_interval(update_points,1)
+pyglet.clock.schedule_interval(add_obstacle, 2)
+pyglet.clock.schedule_interval(update, 1/60)
 pyglet.app.run()
--- hello-test.py
+++ hello-test.py
@@ -1,5 +1,5 @@
 import pyglet
-window = pyglet.window.Window(width=960, height=540)
+window = pyglet.window.Window(width=950, height=540)
 label = pyglet.text.Label('Hello, Jon Ezra', font_size=36, x=20, y=490)
 
 @window.event
